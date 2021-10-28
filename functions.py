e4fea85b11f01d8f48a8c89fc7d0aaca9884012c
aomine001 <pjakobczak@sp.rzgow.pl>
2021-10-28 16:18:55 +0200

    Added all the functions to separate file

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
 import sqlite3
+import uuid
+import os
 from requests import get
 
 def userexists(user):
@@ -22,3 +24,99 @@ def userexists(user):
 def getip():
     ip = get('https://api.ipify.org').text
     return ip
+def CREATE_SESSION(ip, username):
+    this_ip = ip
+    this_username = username
+    this_uuid = uuid.uuid1()
+
+    SESSION_CONNECTION=sqlite3.connect('SESSION.db')
+    SESSION_CURSOR=SESSION_CONNECTION.cursor()
+
+    sql = 'INSERT INTO session (ip,uuid,user) VALUES ("' + this_ip + '","' + str(this_uuid) + '","' + this_username +'");'
+
+    SESSION_CURSOR.execute(sql)
+    SESSION_CONNECTION.commit()
+
+    print("Logged in with success!")
+
+    os.system('python login.py')
+    exit()
+
+    SESSION_CONNECTION.close()
+def logout(ip):
+    this_ip = ip
+
+    SESSION_CONNECTION=sqlite3.connect('SESSION.db')
+    SESSION_CURSOR=SESSION_CONNECTION.cursor()
+
+    sql = 'DELETE FROM session WHERE ip="' + this_ip + '";'
+
+
+
+    SESSION_CURSOR.execute(sql)
+    SESSION_CONNECTION.commit()
+
+    print("Logged out.")
+    SESSION_CONNECTION.close()
+
+    os.system('python login.py')
+    exit()
+
+
+    sql2 = 'SELECT * FROM users WHERE log="' + username + '"'
+
+    userscursor.execute(sql2)
+    usersdb.commit()
+
+    rows2 = userscursor.fetchall()
+
+    try:
+        ban = rows2[0][2]
+    except:
+        pass
+
+    try:
+        if ban == "yes":
+            print("THIS USER IS BANNED!")
+            print("LOGGING OUT THE USER!")
+            _=input()
+            logout(ip)
+            usersdb.close()
+            SESSION_CONNECTION.close()
+            exit()
+    except UnboundLocalError:
+        pass
+
+    usersdb.close()
+
+    if len(rows) != 0:
+        return False
+    else:
+        return True
+
+    SESSION_CONNECTION.close()
+def CHECK_FOR_SESSION(ip):
+    username = ''
+
+    usersdb = sqlite3.connect('users.db')
+    userscursor = usersdb.cursor()
+
+    this_ip = ip
+    SESSION_CONNECTION=sqlite3.connect('SESSION.db')
+    SESSION_CURSOR=SESSION_CONNECTION.cursor()
+
+    sql = 'SELECT * FROM session WHERE ip="' + this_ip + '"'
+
+    SESSION_CURSOR.execute(sql)
+    SESSION_CONNECTION.commit()
+
+    rows = SESSION_CURSOR.fetchall()
+    try:
+        username = rows[0][2]
+    except IndexError:
+        pass
+
+    if len(rows) != 0:
+        return True
+    else:
+        return False
--- login.py
+++ login.py
@@ -1,110 +1,14 @@
 import sqlite3
 import hashlib
-import uuid
 import os
 import time
 import functions
+
 os.system('cls')
 
 ip = functions.getip()
 
 
-def CREATE_SESSION(ip, username):
-    this_ip = ip
-    this_username = username
-    this_uuid = uuid.uuid1()
-
-    SESSION_CONNECTION=sqlite3.connect('SESSION.db')
-    SESSION_CURSOR=SESSION_CONNECTION.cursor()
-
-    sql = 'INSERT INTO session (ip,uuid,user) VALUES ("' + this_ip + '","' + str(this_uuid) + '","' + this_username +'");'
-
-    SESSION_CURSOR.execute(sql)
-    SESSION_CONNECTION.commit()
-
-    print("Logged in with success!")
-
-    os.system('python login.py')
-    exit()
-
-    SESSION_CONNECTION.close()
-def logout(ip):
-    this_ip = ip
-
-    SESSION_CONNECTION=sqlite3.connect('SESSION.db')
-    SESSION_CURSOR=SESSION_CONNECTION.cursor()
-
-    sql = 'DELETE FROM session WHERE ip="' + this_ip + '";'
-
-
-
-    SESSION_CURSOR.execute(sql)
-    SESSION_CONNECTION.commit()
-
-    print("Logged out.")
-    SESSION_CONNECTION.close()
-
-    os.system('python login.py')
-    exit()
-
-
-    sql2 = 'SELECT * FROM users WHERE log="' + username + '"'
-
-    userscursor.execute(sql2)
-    usersdb.commit()
-
-    rows2 = userscursor.fetchall()
-
-    try:
-        ban = rows2[0][2]
-    except:
-        pass
-
-    try:
-        if ban == "yes":
-            print("THIS USER IS BANNED!")
-            print("LOGGING OUT THE USER!")
-            _=input()
-            logout(ip)
-            usersdb.close()
-            SESSION_CONNECTION.close()
-            exit()
-    except UnboundLocalError:
-        pass
-
-    usersdb.close()
-
-    if len(rows) != 0:
-        return False
-    else:
-        return True
-
-    SESSION_CONNECTION.close()
-def CHECK_FOR_SESSION(ip):
-    username = ''
-
-    usersdb = sqlite3.connect('users.db')
-    userscursor = usersdb.cursor()
-
-    this_ip = ip
-    SESSION_CONNECTION=sqlite3.connect('SESSION.db')
-    SESSION_CURSOR=SESSION_CONNECTION.cursor()
-
-    sql = 'SELECT * FROM session WHERE ip="' + this_ip + '"'
-
-    SESSION_CURSOR.execute(sql)
-    SESSION_CONNECTION.commit()
-
-    rows = SESSION_CURSOR.fetchall()
-    try:
-        username = rows[0][2]
-    except IndexError:
-        pass
-
-    if len(rows) != 0:
-        return True
-    else:
-        return False
 def login():
 
     useroption_login = input("Type here login")
@@ -127,9 +31,9 @@ def login():
     if length != 0:
         print('Logging in...')
 
-        if CHECK_FOR_SESSION(ip) != True:
+        if functions.CHECK_FOR_SESSION(ip) != True:
 
-            CREATE_SESSION(ip, useroption_login)
+            functions.CREATE_SESSION(ip, useroption_login)
         else:
             os.system('python login.py')
             exit()
@@ -142,7 +46,7 @@ def login():
 
     connection.close()
 def check():
-    if CHECK_FOR_SESSION(ip) != False:
+    if functions.CHECK_FOR_SESSION(ip) != False:
         print("hello!")
         while True:
             print("-------------")
@@ -153,7 +57,7 @@ def check():
 
 
             if com == str(1):
-                logout(ip)
+                functions.logout(ip)
             elif com == str(2):
                 exit()
             else:
